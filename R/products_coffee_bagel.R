884849ded7350f634a072d49c11c5d8c1f3a8e84
Julian During <duringjulian@gmail.com>
2023-01-06 12:09:45 +0000

    Better function parameter name

--- R/products_coffee_bagel.R
+++ R/products_coffee_bagel.R
@@ -1,4 +1,4 @@
-products_coffee_bagel <- function(df_products_raw, coffee_bagel_regex) {
-  df_products_raw |>
+products_coffee_bagel <- function(df_products, coffee_bagel_regex) {
+  df_products |>
     filter(str_detect(desc, coffee_bagel_regex))
 }
