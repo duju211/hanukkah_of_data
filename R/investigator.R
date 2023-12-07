14627c33c88944303989461a0a247249e0d35396
Julian During <duringjulian@gmail.com>
2023-12-07 19:57:25 +0100

    Day1 2023

--- R/investigator.R
+++ R/investigator.R
@@ -1,15 +1,7 @@
-investigator <- function(df_customers, df_phone_letter) {
-  df_customers_pro <- df_customers |>
-    transmute(
-      name_pro = str_split(str_to_lower(str_remove_all(
-        last_name, "\\s"), "\\s"), ""),
-      customerid) |>
-    unnest(name_pro) |>
-    left_join(df_phone_letter, by = c("name_pro" = "letter")) |>
-    group_by(customerid) |>
-    summarise(phone_pro = str_flatten(nr))
-  
-  df_customers |>
-    left_join(df_customers_pro, by = "customerid") |>
-    filter(str_length(str_extract(phone_chr, phone_pro)) == str_length(phone_chr))
+investigator <- function(df_last_names, df_phone_letter) {
+  df_last_names |>
+    left_join(df_phone_letter, by = "letter") |>
+    group_by(customerid, phone_chr) |>
+    summarise(phone_pro = str_flatten(nr), .groups = "drop") |>
+    filter(phone_chr == phone_pro)
 }
